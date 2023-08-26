a903ba2eb18c1e82308a3ae59ce8d378616d9146
YAS-OKA <108788135+YAS-OKA@users.noreply.github.com>
2023-08-27 00:22:44 +0900

    プロジェクト ファイルを追加します。

---
+++ CreateMotion/AnimationSystem.h
@@ -0,0 +1,334 @@
+﻿#pragma once
+
+# include <Siv3D.hpp> // OpenSiv3D v0.6.6
+
+class Movable {
+public:
+	Vec2 pos;
+	SizeF size;
+	double angle;
+	double z;
+	Vec2 rotatePos;
+	ColorF color;
+	bool mirror;
+
+	Movable(const Vec2& pos, const SizeF& size, const Vec2& rotatePos = {}, double angle = 0_deg, double z = 0, const ColorF& color = ColorF{ 1 }, bool mirror = false)
+		:pos{ pos }, size{ size }, angle{ angle }, z{ z }, rotatePos{ rotatePos }, color{ color }, mirror{ mirror } {}
+
+	virtual void draw()const = 0;
+};
+
+class Character;
+
+
+class Move {
+public:
+	Move() {}
+
+	virtual void start(Character* character) {};
+
+	virtual void update(Character* character, double t = Scene::DeltaTime()) = 0;
+
+	virtual bool isActive() = 0;
+};
+
+class Motion :public Move {
+public:
+
+	Array<Move*>list;
+
+	double speed = 1;
+	bool parallel = false;
+	bool loop = false;
+
+	Motion(const Array<Move*>& list, bool parallel = false, bool loop = false)
+		:list{ list }, parallel{ parallel }, loop{ loop } {
+
+
+	}
+
+	Motion() {}
+
+	void clear() {
+		list.clear();
+		index = 0;
+		active = false;
+		speed = 1;
+		parallel = false;
+		loop = false;
+	}
+
+	void setSpeed(double _speed) {
+		speed = _speed;
+	}
+
+	void add(Move* move) {
+		list << move;
+	}
+
+	void start(Character* character)override {
+		index = 0;
+		if (parallel) {
+			for (auto& move : list) {
+				move->start(character);
+			}
+		}
+		else {
+			list[0]->start(character);
+		}
+	}
+
+	void update(Character* character, double t = Scene::DeltaTime())override {
+
+		Print << list.size();
+
+		if (parallel) {
+
+			active = false;
+
+			for (auto& move : list) {
+				if (move->isActive()) {
+					move->update(character, t * speed);
+					active |= true;
+				}
+			}
+		}
+		else {
+			if (checkIndex()) {
+				list[index]->update(character, t * speed);
+				if (not list[index]->isActive()) {
+					index++;
+					if (checkIndex()) {
+						list[index]->start(character);
+					}
+					//終了なら
+					else {
+						active = false;
+					}
+				}
+			}
+		}
+
+		if (not active && loop) {
+			start(character);
+			active = true;
+		}
+	}
+
+	bool isActive()override {
+		return active;
+	}
+
+private:
+	size_t index = 0;
+	bool active = false;
+
+	bool checkIndex() {
+		return index < list.size();
+	}
+};
+
+class DrawManager {
+public:
+
+	Array<Movable*>movables;
+
+	DrawManager() {}
+
+	DrawManager(const Array<Movable*>& movables) :movables{ movables } {}
+
+	void add(Movable* movable) {
+		movables << movable;
+	}
+
+	void update() {
+		movables.stable_sort_by([](Movable* left, Movable* right) {return left->z < right->z; });
+	}
+
+	void draw()const {
+		movables.each([](Movable* movable) {movable->draw(); });
+	}
+
+	void clear() {
+		movables.clear();
+	}
+
+};
+
+class Joint :public Movable {
+public:
+
+	String textureName;
+
+	Array<Joint*>joints;
+
+	Mat3x2 mat = Graphics2D::GetLocalTransform();
+
+	Joint(const Vec2& center, const Vec2& rotatePos, const String& textureName,double z=0)
+		:Movable{ center,TextureAsset{textureName}.size(),rotatePos,0_deg,0 }, textureName{ textureName } {}
+
+	void add(Joint* joint) {
+		joints << joint;
+	}
+
+	//Moveのあと
+	void update() {
+		const Transformer2D t1{ Mat3x2::Translate(pos) };//中心をずらす
+		const Transformer2D t2{ Mat3x2::Rotate(angle,rotatePos) };//回転
+		mat = Graphics2D::GetLocalTransform();//変換行列を保存
+		for (auto& joint : joints) {
+			joint->update();
+		}
+	}
+
+	void draw()const override {
+		{
+			Transformer2D t{ mat };
+			TextureAsset{ textureName }.resized(size).drawAt({ 0,0 }, color);
+			(rotatePos).asCircle(2).draw(Palette::Red);
+		}
+	}
+
+	double getMaxY() {
+		RectF rect{ Arg::center(0,0),size };
+		double max = -Math::Inf;
+		for (auto i : step(4)) {
+			max = Max(mat.transformPoint(rect.point(i)).y, max);
+		}
+		return max;
+	}
+};
+
+class Character {
+public:
+
+	struct Body {
+		std::unique_ptr<Joint>joint;
+		String parentName;
+	};
+
+	Array<Motion>motions;
+
+	double angle = 0_deg;
+	bool mirror = false;
+	double scale = 1.0;
+
+	HashTable<String, Body>table;
+
+	Joint* joint = nullptr;
+
+	Character(double scale = 1.0) :scale{ scale } {}
+
+	Character(const JSON& json, double scale = 1.0) :scale{ scale } {
+
+		for (const auto& body : json[U"Body"])
+		{
+			String name = body.key;
+			const auto& object = json[U"Body"][name];
+			set(name, object[U"Parent"].getString(), object[U"Position"].get<Vec2>(), object[U"RotateCenter"].get<Vec2>(), object[U"TexturePath"].getString(), object[U"Z"].get<double>());
+		}
+		complete();
+	}
+
+	void set(const String& name, const String& parentName, const Vec2& center, const Vec2& rotatePos, const String& textureName,double z) {
+		TextureAsset::Register(textureName, textureName);
+		table[name] = { std::make_unique<Joint>(center, rotatePos, textureName,z),parentName };
+	}
+
+	void complete() {
+		for (auto it = table.begin(); it != table.end(); ++it)
+		{
+			if (it->second.parentName == U"__Main__") {
+				joint = it->second.joint.get();
+			}
+			else {
+				if (table.contains(it->second.parentName)) {
+					table[it->second.parentName].joint->add(it->second.joint.get());
+				}
+			}
+		}
+	}
+
+	void add(const String& name, const String& parentName, const Vec2& center, const Vec2& rotatePos, const String& textureName) {
+		table[name] = { std::make_unique<Joint>(center, rotatePos, textureName),parentName };
+		table[parentName].joint->add(table[name].joint.get());
+	}
+
+	void update(double dt=Scene::DeltaTime()) {
+		
+		for (auto it = motions.begin(); it != motions.end();)
+		{
+			if (not it->isActive())
+			{
+				it = motions.erase(it);
+			}
+			else
+			{
+				it->update(this,dt);
+				++it;
+			}
+		}
+		Transformer2D trans{ Mat3x2::Scale(Cos(angle) * scale,scale,joint->pos) };
+		joint->update();
+	}
+
+	void addMotion(const Motion& motion) {
+		motions << motion;
+		motions.back().start(this);
+	}
+
+	Joint* get(const String& name) {
+		return table[name].joint.get();
+	}
+
+	void setDrawManager(DrawManager* manager) {
+		for (auto it = table.begin(); it != table.end(); ++it)
+		{
+			manager->add(it->second.joint.get());
+		}
+	}
+
+	void touchGround(double y) {
+		joint->pos.y += y - getMaxY();
+	}
+
+	double getMaxY() {
+		double max = -Math::Inf;
+		for (auto it = table.begin(); it != table.end(); ++it)
+		{
+			max = Max(it->second.joint->getMaxY(), max);
+		}
+		return max;
+	}
+};
+
+
+class TimeMove :public Move {
+public:
+	double time = 0;
+	double timelim;
+
+	TimeMove(double timelim = 1) : timelim{ timelim } {}
+
+	virtual void start(Character* character)override {
+		time = 0;
+	}
+
+	virtual void update(Character* character,double t = Scene::DeltaTime())override = 0;
+
+	double calTime(double dt) {
+		//もし制限時間を超えるなら
+		if (timelim < time + dt) {
+			dt = timelim - time;
+			time = timelim;
+		}
+		else {
+			time += dt;
+		}
+		return dt;
+	}
+
+	virtual bool isActive() {
+		return time < timelim;
+	}
+};
---
+++ CreateMotion/Main.cpp
@@ -0,0 +1,184 @@
+﻿# include <Siv3D.hpp> // OpenSiv3D v0.6.6
+#include"AnimationSystem.h"
+
+//設定した角度回転
+class Rotate :public TimeMove {
+public:
+	double rad;
+	String target;
+
+	Rotate(const String target, double rad, double timelim) :TimeMove{ timelim }, target{ target }, rad{ rad } {}
+
+	void update(Character* character, double dt = Scene::DeltaTime())override {
+		dt = calTime(dt);
+		character->get(target)->angle += dt * rad / timelim;
+	}
+};
+
+class SetAngle :public Move {
+public:
+	double rad;
+	String target;
+	SetAngle(const String& target, double rad) :target{ target }, rad{ rad } {}
+
+	void start(Character* character)override {
+		character->get(target)->angle = rad;
+	}
+
+	void update(Character* character, double dt = Scene::DeltaTime())override {}
+
+	bool isActive()override {
+		return false;
+	}
+};
+
+
+class Mirror :public TimeMove {
+public:
+	double rate;
+	int32 mirror;
+	bool trueMirror = false;
+	/// @brief 
+	/// @param mirror -1左　0反転　1右
+	/// @param rate 
+	Mirror(int32 mirror, double rate = 360_deg) :TimeMove{ 180_deg / rate }, rate{ rate }, mirror{ mirror } {
+	}
+
+	void start(Character* character)override {
+		TimeMove::start(character);
+
+		if (mirror == 0) {
+			character->mirror = not character->mirror;
+			trueMirror = not character->mirror;
+		}
+		else if (0 < mirror) {
+			character->mirror = false;
+			trueMirror = false;
+		}
+		else {
+			character->mirror = true;
+			trueMirror = true;
+		}
+	}
+
+	void update(Character* character, double dt = Scene::DeltaTime())override {
+		dt = calTime(dt);
+		if (trueMirror) {
+			character->angle += dt * rate;
+		}
+		else {
+			character->angle -= dt * rate;
+		}
+	}
+};
+
+//設定した秒数待つ
+class Wait :public TimeMove {
+public:
+	Wait(double timelim) :TimeMove{ timelim } {}
+
+	void update(Character* character, double dt = Scene::DeltaTime())override {
+		dt = calTime(dt);
+	}
+};
+
+class MotionLoader {
+public:
+
+	CSV csv;
+	HashTable<String, std::function<Move* (const Array<String>&)>>moveResolver;
+
+	MotionLoader(const CSV& csv) :csv{ csv } {
+		resetResolver();
+
+	}
+
+	void resetResolver() {
+		moveResolver[U"Rotate"] = [](const Array<String>& list) {return new Rotate{ list[1],Parse<double>(list[2]) * 1_deg,Parse<double>(list[3]) }; };
+		moveResolver[U"Wait"] = [](const Array<String>& list) {return new Wait{ Parse<double>(list[1]) }; };
+	}
+
+	Motion LoadMotion(String region = U"") {
+
+		Motion motion;
+
+		CSV tmpCsv = csv;
+
+		size_t index = 0;
+
+		if (region != U"") {
+			for (size_t row = 0; row < tmpCsv.rows(); ++row)
+			{
+				if ((tmpCsv.columns(row) != 0) && tmpCsv[row][0] == region) {
+					index = row + 1;
+					break;
+				}
+			}
+		}
+
+		HashTable<String, String>variableTable;
+
+		Motion* tmpMotion = new Motion{};
+		tmpMotion->parallel = true;
+
+		//size_t - int =size_t なので tmpCsv.rows()が0のときほぼ無限ループになる。
+		//場合わけで解決？ -1いらなくない？
+		for (size_t row = index; row < tmpCsv.rows(); ++row)
+		{
+			if (tmpCsv.columns(row) == 0) {
+				motion.add(tmpMotion);
+				tmpMotion = new Motion{};
+				tmpMotion->parallel = true;
+			}
+			else if (tmpCsv[row][0][0] == U'#') {
+				break;
+			}
+			else if (tmpCsv[row][0][0] == U'@') {
+				variableTable[tmpCsv[row][0]] = tmpCsv[row][1];
+			}
+			else {
+				for (size_t col = 0; col < tmpCsv.columns(row); ++col) {
+					//変数を数値に置き換え
+					if (variableTable.contains(tmpCsv[row][col])) {
+						tmpCsv[row][col] = variableTable[tmpCsv[row][col]];
+					}
+				}
+				tmpMotion->add(moveResolver[tmpCsv[row][0]](tmpCsv[row]));
+			}
+		}
+		//あとで考える
+		motion.add(tmpMotion);
+		return motion;
+	}
+};
+
+void Main()
+{
+	//==========体の用意==========
+	Character character{ JSON::Load(U"tim-kun/test.json"),2 };
+
+	MotionLoader loader{ CSV{U"motion.txt"} };
+
+	Motion motion = loader.LoadMotion();
+
+	//==========描画に追加==========
+	DrawManager manager;
+	character.setDrawManager(&manager);
+
+	while (System::Update())
+	{
+		//if (MouseL.down()) {
+		//	MotionLoader loader{ CSV{U"motion.txt"} };
+		//	character.addMotion(loader.LoadMotion());
+		//}
+		motion.update(&character);
+
+		character.update();
+		character.touchGround(500);
+
+		manager.draw();
+
+		RectF{ 0,500,800,100 }.draw(Palette::Green);
+	}
+}
+
